c04a21072e077bf2ca796633f0ef9ac4563477c2
xpdu <443173414@qq.com>
2019-06-02 01:55:02 +0800

    Implementing searching function

--- filecounter.py
+++ filecounter.py
@@ -1,15 +1,17 @@
 #This tiny tool is used to count the number of specified type file.
 import sys
 import os
+import re
 
 class Config(object):
 
     def __init__(self):
         # options config, (key, needparam, desc)
-        self.OPTION_APPOINTTYPE = ("-a", True, "appoint the type of files")
-        self.OPTION_EXCLUDEFILE = ("-e", True, "exclude directorys, regular expression is avliable")
-        self.OPTION_LEVELS      = ("-l", True, "the levels when scan directory recursively")
+        self.OPTION_APPOINTTYPE = ("-a", True,  "appoint the type of files")
+        self.OPTION_EXCLUDEFILE = ("-e", True,  "exclude directorys, regular expression is avliable")
+        self.OPTION_LEVELS      = ("-l", True,  "the levels when scan directory recursively")
         self.OPTION_HELP        = ("-h", False, "for help")
+        self.OPTION_SEARCH      = ("-s", True,  "search pattern within found files")
 
     def getKey(self, option):
         key, _, _ = option
@@ -82,6 +84,44 @@ def filter(file, options):
             return True
     return False
 
+#search pattern in found files
+def search(files, options):
+    if not options.has_key(config.getKey(config.OPTION_SEARCH)):
+        return
+    pstr = options[config.getKey(config.OPTION_SEARCH)]
+    pattern = re.compile(pstr)
+    for filename in files:
+        results = []
+        file = open(filename, mode="r+")
+        linenumber = 1
+        line = file.readline(1024*512)
+        while line != "":
+            match = pattern.finditer(line)
+            matchpos = []
+            for m in match:
+                matchpos.append((m.start(), m.group()))
+            if len(matchpos) > 0:
+                results.append((linenumber, line, matchpos))
+            line = file.readline(1024*512)
+            linenumber += 1
+
+        # finish search this file, print result if have any
+        if len(results) == 0:
+            continue
+        print filename
+        for linenumber, line, positions in results:
+            writedend = 0
+            outline = ""
+            for start, group in positions:
+                if writedend < start:
+                    outline += line[writedend:start]
+                outline += ("\033[0;31m" + group + "\033[0m")
+                writedend = start + len(group)
+            if writedend < len(line):
+                outline += line[writedend:]
+            print str(linenumber) + "   " + outline,
+        file.close()
+
 #handle count task
 def handle(path, options):
     files = []
@@ -96,10 +136,14 @@ def handle(path, options):
     else:
         files = scanfiles(path, 1, sys.maxint, options)
     
-    #print results
-    for item in files:
-        print item
-    print "found " + str(len(files)) + " results"
+    #if there is no search option, print result
+    if not options.has_key(config.getKey(config.OPTION_SEARCH)):
+        for item in files:
+            print item
+        print "found " + str(len(files)) + " results"
+        return
+    else:
+        search(files, options)
 
 #Parse argv
 def parse(argv):
